c9ab624288c32432b8605a864aadf71a89db0c15
Brian Picciano <bgp2396@bellsouth.net>
2014-03-08 01:25:58 -0500

    updated Lazy docs

--- lazy.go
+++ lazy.go
@@ -1,5 +1,11 @@
 package seq
 
+// A Lazy is an implementation of a Seq which only actually evaluates its
+// contents as those contents become needed. Lazys can be chained together, so
+// if you have three steps in a pipeline there aren't two intermediate Seqs
+// created, only the final resulting one. Lazys are also thread-safe, so
+// multiple routines can interact with the same Lazy pointer at the same time
+// but the contents will only be evalutated once.
 type Lazy struct {
 	this  interface{}
 	next  *Lazy
@@ -7,11 +13,7 @@ type Lazy struct {
 	ch    chan struct{}
 }
 
-
-// A Lazy is an implementation of a Seq which only actually evaluates its
-// contents as those contents become needed. Lazys can be chained together, so
-// if you have three steps in a pipeline there aren't two intermediate Seqs
-// created, only the final resulting one.
+// Given a Thunk, returns a Lazy around that Thunk.
 func NewLazy(t Thunk) *Lazy {
 	l := &Lazy{ch: make(chan struct{})}
 	go func(){
