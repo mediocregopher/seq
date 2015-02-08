0f1ece5a03b0f3fd44e3f85acf6233af1d04cefc
Brian Picciano <mediocregopher@gmail.com>
2015-02-08 16:36:28 -0500

    formatting changes to example_thunk_test

--- example_thunk_test.go
+++ example_thunk_test.go
@@ -1,27 +1,24 @@
 package seq
 
-import (
-	"fmt"
-	"github.com/mediocregopher/seq"
-)
+import "fmt"
 
 // numberThunk is used to create a sequence of inifinte, sequential integers
-func numberThunk(i int) seq.Thunk {
-	return func() (interface{}, seq.Thunk, bool) {
+func numberThunk(i int) Thunk {
+	return func() (interface{}, Thunk, bool) {
 		return i, numberThunk(i + 1), true
 	}
 }
 
 // Numbers is a nice wrapper around numberThunk which creates an root thunk and
 // wraps it with a Lazy
-func Numbers() *seq.Lazy {
+func Numbers() *Lazy {
 	rootThunk := numberThunk(0)
-	return seq.NewLazy(rootThunk)
+	return NewLazy(rootThunk)
 }
 
 func ExampleThunk() {
 	var el interface{}
-	var s seq.Seq = Numbers()
+	var s Seq = Numbers()
 	var ok bool
 	for i := 0; i < 10; i++ {
 		if el, s, ok = s.FirstRest(); ok {
